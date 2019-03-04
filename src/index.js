b141fbd0f4b39440228651fedc4ef01bebcc3806
Andres Mena <ballerlikemike23@gmail.com>
2019-03-04 00:38:34 -0500

    Add Firebase

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import LandingPage from './LandingPage/LandingPage.js';
-import * as serviceWorker from './serviceWorker';
+import { firebase } from './firebase/firebase'
 
 ReactDOM.render(<LandingPage />, document.getElementById('root'));
-serviceWorker.unregister();
