6e817a5021d97ada3c1171fba0126f4c26e96f32
Andres Mena <ballerlikemike23@gmail.com>
2019-03-04 00:32:46 -0500

    Update Folders Structure

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase'
+
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+}
+
+firebase.initalizeApp(config)
